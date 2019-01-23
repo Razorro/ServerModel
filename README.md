94c2de71345bd4aa5abedc2cc065e87ceeab96e3
JuliusBelmont <623571826@qq.com>
2019-01-23 14:28:45 +0800

    coroutine_server

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ServerModel
 *Implement some simple server net message dealing model, as the practise of from knowledge to engineering*
+
+## Model List
+- coroutine_server: IO multiplex + nonblock, no other fantastic things, but use coroutine to deal with
+buffer, that makes a bit easier and make a more deeper grasp of Python coroutine.
+  
+### coroutine_server
+Simply use select, as an IO multiplex model to make the server has the concurrenct ability, 
+use coroutine as the socket's read and write buffer.
---
+++ coroutine_server.py
@@ -0,0 +1,152 @@
+import socket
+import select
+import copy
+from functools import wraps
+from collections import deque
+
+
+def coroutine(func):
+    """ Decorator: primes `func` by advancing to first `yield` """
+    @wraps(func)
+    def primer(*args, **kwargs):
+        gen = func(*args, **kwargs)
+        next(gen)
+        return gen
+    return primer
+
+
+class Server:
+    def __init__(self, ip, port, maxConn=100):
+        self.ip = ip
+        self.port = port
+        self.selectFd = None
+        self.sock = None
+        self.readChannel = {}                           # read channel
+        self.writeChannel = {}                          # write channel
+        self.pendingPacket = {}                         # pending to deal
+        self.conn = {}                                  # connnection manager
+        self.wlist = set()                              # wait to write
+        self.maxConn = maxConn
+
+    def __repr__(self):
+        return "listen on %s:%s, current connection num: %s" % (self.ip, self.port, len(self.conn))
+
+    @staticmethod
+    def packetTrimmer(byteBuf):
+        """ Default method to trim a packet out of buffer """
+        while True:
+            datas = yield
+            byteBuf += datas
+            if len(byteBuf) >= 2:
+                break
+
+        packetSize = int(byteBuf[:2].decode())
+        while True:
+            datas = yield
+            byteBuf += datas
+            if len(byteBuf) >= packetSize+2:
+                break
+
+        packetData = byteBuf[2:packetSize+2]
+        return packetData, byteBuf[2+packetSize:]
+
+    def packetProcessing(self):
+        """ Dealing process, customize it as whatever you want """
+        for sock, msgQueue in self.pendingPacket.items():
+            if len(msgQueue) == 0:
+                continue
+
+            packet = msgQueue.popleft()
+            """
+            calculating ...
+            """
+            self.wlist.add(sock)                                # turn on watching on write socket
+            self.writeChannel[sock].send(packet)
+
+    def start(self):
+        print('start server at %s:%s' % (self.ip, self.port))
+        try:
+            self.sock = socket.socket()
+            self.sock.bind((self.ip, self.port))
+            self.sock.listen(self.maxConn)
+        except Exception as e:
+            print('initialize server error: %s' % e)
+            return
+        else:
+            while True:
+                rlist = [self.sock] + list(self.readChannel.keys())
+                wlist = copy.copy(list(self.wlist))
+                self.packetProcessing()
+                readSet, writeSet, exceptSet = select.select(rlist, wlist, [])
+
+                for readFd in readSet:
+                    if readFd == self.sock:
+                        self.createConnection()
+                    else:
+                        self.readHandler(readFd)
+
+                for writeFd in writeSet:
+                    self.writeHandler(writeFd)
+
+                for exceptFd in exceptSet:
+                    print('got an exception notification about fd %s' % exceptFd)
+
+    def createConnection(self):
+        conn, addr = self.sock.accept()
+        print("establish connection from %s" % (conn.getpeername(),))
+        self.genereateChannel(conn)
+
+    def readHandler(self, sock):
+        """
+        For best match with hardware and network realities, the value of bufsize should be
+        a relatively small power of 2, for example, 4096.
+        """
+        nbytes = sock.recv(4096)
+        if len(nbytes) == 0:
+            print('conn from %s closed' % (sock.getsockname(),))
+            del self.writeChannel[sock]
+            del self.readChannel[sock]
+            if sock in self.pendingPacket: del self.pendingPacket[sock]
+            sock.close()
+        else:
+            print('get data from sock%s --> %s' % (sock.getpeername(), nbytes.decode()))
+            self.readChannel[sock].send(nbytes)
+
+    def writeHandler(self, sock):
+        pass
+
+    def closeConnection(self, sock):
+        pass
+
+    def genereateChannel(self, sock):
+        """ Create channel for socket """
+        self.readChannel[sock] = self.getReadChannel(sock)
+        self.writeChannel[sock] = self.getWriteChannel(sock)
+
+    @coroutine
+    def getReadChannel(self, sock):
+        readBuf = bytearray()
+        while True:
+            datas = yield from self.packetTrimmer(readBuf)
+            packet = datas[0]
+            readBuf = datas[1]
+            print('trim message from sock%s --> %s' % (sock.getpeername(), packet.decode()))
+            self.pendingPacket.setdefault(sock, deque()).append(packet)
+
+    @coroutine
+    def getWriteChannel(self, sock):
+        writeBuf = bytearray()
+        while True:
+            data = yield
+            writeBuf += data
+            nwritten = sock.send(writeBuf)
+            writeBuf = writeBuf[nwritten:]
+
+            if len(writeBuf) == 0:
+                print('cancel write listening')
+                self.wlist.remove(sock)                 # turn off watching on write socket
+
+
+if __name__ == "__main__":
+    s = Server('localhost', 10010)
+    s.start()
